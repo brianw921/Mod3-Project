5da42461e21a64292be18af032c7e351171f5674
Brian Wong <bwong92@gmail.com>
2019-10-22 11:31:41 -0400

    movie migration

--- db/migrate/20191020173527_create_movies.rb
+++ db/migrate/20191020173527_create_movies.rb
@@ -2,6 +2,17 @@ class CreateMovies < ActiveRecord::Migration[6.0]
   def change
     create_table :movies do |t|
       t.string :title
+      t.string :poster
+      t.string :actors 
+      t.string :director
+      t.string :released
+      t.string :genre
+      t.string :rated
+      t.string :run_time
+      t.string :plot 
+      t.string :production
+      t.string :type
+
       t.timestamps
     end
   end
